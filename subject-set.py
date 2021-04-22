0d00333078b2476a40d7f357d5cde3ffffc1e20d
Jim O'Donnell <jim@zooniverse.org>
2021-04-22 15:41:12 +0100

    Enumerate all subject sets for a project

--- subject-set.py
+++ subject-set.py
@@ -1,24 +1,30 @@
 import csv
-from panoptes_client import SubjectSet
+from panoptes_client import Project, SubjectSet
 
-subject_set=SubjectSet.find(92753)
-index_fields = (subject_set.metadata['indexFields']).split(',')
+project = Project.find(12268)
 
-headers = ['subject_id']
-for field in index_fields:
-    headers.append(field)
+for subject_set in project.links.subject_sets:
+    print subject_set.id
+    if 'indexFields' in subject_set.metadata:
+        print(subject_set.metadata['indexFields'])
+        index_fields = (subject_set.metadata['indexFields']).split(',')
 
-rows = []
-for subject in subject_set.subjects:
-    row = {}
-    row['subject_id'] = subject.id
-    for field in index_fields:
-        row[field] = subject.metadata[field]
-    rows.append(row)
+        headers = ['subject_id']
+        for field in index_fields:
+            headers.append(field)
 
-with open('data/92553.csv', mode='w') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=headers)
-    writer.writeheader()
+        rows = []
+        for subject in subject_set.subjects:
+            row = {}
+            row['subject_id'] = subject.id
+            for field in index_fields:
+                row[field] = subject.metadata[field]
+            rows.append(row)
 
-    for row in rows:
-        writer.writerow(row)
+        filename = 'data/{id}.csv'.format(id=subject_set.id)
+        with open(filename, mode='w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=headers)
+            writer.writeheader()
+
+            for row in rows:
+                writer.writerow(row)
