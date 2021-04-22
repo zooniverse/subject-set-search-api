7bc1f3590e276f05322bdf6174c360a822d50d2a
Jim O'Donnell <jim@zooniverse.org>
2021-04-22 17:49:10 +0100

    Add import script and settings file

---
+++ import-csv-files-to-sqlite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+# remove any existing (ephemeral) db as we're going to rebuild it
+rm -f "./databases/*.db"
+
+mkdir ./databases
+
+# run the import csv cmd using csvs-to-sqlite
+echo ---
+echo "Importing ./*.csv to db: ./databases/subjects.db"
+csvs-to-sqlite --replace-tables ./*.csv ./databases/subjects.db
+
+# inspect the databases to create and inspect file
+# used in publishing the database files via datasette
+# https://docs.datasette.io/en/latest/settings.html?highlight=immutable#configuration-directory-mode
+# https://docs.datasette.io/en/latest/performance.html?highlight=inspect#using-datasette-inspect
+# ensure we build this file from the dir of the databases for immutables config json
+cd ./databases/
+datasette inspect *.db --inspect-file=inspect-data.json
+cd -
