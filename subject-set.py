621a1de0052bf0f3479b9c5321c2e159f5b65421
Campbell Allen <campbell.allen@gmail.com>
2021-09-29 17:57:11 +0100

    allow easier addition of new projects
    ensure we can easily add new project subject set data to the API

--- subject-set.py
+++ subject-set.py
@@ -1,35 +1,39 @@
 import csv
 from panoptes_client import Project, SubjectSet
 
-project = Project.find(12268)
+# update this list of project ids
+# to build linked subject sets data files
+# that will be included in the API
+project_ids = [ 12268 ]
 
-for subject_set in project.links.subject_sets:
-    print(subject_set.id)
-    if 'indexFields' in subject_set.metadata:
-        print(subject_set.metadata['indexFields'])
-        index_fields = (subject_set.metadata['indexFields']).split(',')
+for project in project_ids:
+    for subject_set in project.links.subject_sets:
+        print(subject_set.id)
+        if 'indexFields' in subject_set.metadata:
+            print(subject_set.metadata['indexFields'])
+            index_fields = (subject_set.metadata['indexFields']).split(',')
 
-        headers = ['priority', 'subject_id']
-        for field in index_fields:
-            headers.append(field)
-
-        rows = []
-        for subject in subject_set.subjects:
-            row = {}
-            row['subject_id'] = subject.id
-            row['priority'] = -1
-            if 'priority' in subject.metadata:
-                row['priority'] = subject.metadata['priority']
-            if '#priority' in subject.metadata:
-                row['priority'] = subject.metadata['#priority']
+            headers = ['priority', 'subject_id']
             for field in index_fields:
-                row[field] = subject.metadata[field]
-            rows.append(row)
+                headers.append(field)
 
-        filename = 'data/{id}.csv'.format(id=subject_set.id)
-        with open(filename, mode='w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=headers)
-            writer.writeheader()
+            rows = []
+            for subject in subject_set.subjects:
+                row = {}
+                row['subject_id'] = subject.id
+                row['priority'] = -1
+                if 'priority' in subject.metadata:
+                    row['priority'] = subject.metadata['priority']
+                if '#priority' in subject.metadata:
+                    row['priority'] = subject.metadata['#priority']
+                for field in index_fields:
+                    row[field] = subject.metadata[field]
+                rows.append(row)
 
-            for row in rows:
-                writer.writerow(row)
+            filename = 'data/{id}.csv'.format(id=subject_set.id)
+            with open(filename, mode='w') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=headers)
+                writer.writeheader()
+
+                for row in rows:
+                    writer.writerow(row)
