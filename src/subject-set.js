97f0f182e4ac0fd1d9b710082f61109d33d2a11c
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2025-02-12 15:44:14 -0600

    symmetry

--- src/subject-set.js
+++ src/subject-set.js
@@ -3,7 +3,7 @@ const { unparse } = require('papaparse')
 
 const fetchWithRetry = require('./fetchWithRetry')
 
-const PROJECT_IDS = [12268, 12561, 16957, 5481, 17426, 20163, 24686 ]
+const PROJECT_IDS = [ 12268, 12561, 16957, 5481, 17426, 20163, 24686 ]
 const PAGE_SIZE = 100
 
 function subjectMetadataRow(subject, indexFields = []) {
